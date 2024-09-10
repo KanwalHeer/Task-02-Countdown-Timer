35fe510f966f13e24bc37145de71d1a9611c38af
ali langah <alilangah78@gmail.com>
2024-09-11 00:29:51 +0500

    birthdayCard

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
-import Counter from "./counter/counter";
-export default function Home() {
+import Card from "./card/card"
+export default function Psge() {
   return (
-    <main className="flex flex-col items-center justify-between p-4 md:p-8 lg:p-12">
-      <h1 className="text-xl md:text-2xl lg:text-3xl font-extrabold mb-4 md:mb-6">Weather App</h1>
-      <div className="bg-gray-200 text-black p-4 md:p-6 lg:p-8 shadow-lg shadow-gray-700 rounded-xl w-full max-w-lg">
-        <Counter/>
-      </div> 
-    </main>
-  );
+    <div>
+      <Card/>
+    </div>
+  )
 }
