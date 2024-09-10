03bb3182ab5906017ab5289b9b3eb4826a5f7b38
ali langah <alilangah78@gmail.com>
2024-09-11 01:11:40 +0500

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 
-  import React from 'react'
+  import Countdown from "./counter/counter"
   
   export default function page() {
     return (
       <div>
-        <h1>hello</h1>
+        <Countdown/>
       </div>
     )
   }
